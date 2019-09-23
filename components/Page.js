c28daa41f3f836a9f262122657d65e7fee549404
educartoons <educartoons@gmail.com>
2019-09-22 23:12:17 -0500

    Page Component

---
+++ components/Page.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Page extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hey I'm the page component external</p>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
---
+++ readme.md
@@ -0,0 +1,12 @@
+## Reactjs code snippets
+
+Install this plugin in your Visual Studio Code
+
+In order to create class component
+> rcc 
+
+In order to create arrow function component
+> rac 
+
+In order to create function component
+> rfc 
