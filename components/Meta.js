1b81a5ae9b780d7c371a6f729d0c19bbd80bb8a5
educartoons <educartoons@gmail.com>
2019-09-22 23:32:09 -0500

    Custom Meta Component

---
+++ components/Meta.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+      <meta charset="UTF-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+      <link rel="stylesheet" href="./static/nprogress.css"/>
+      <title>Bamboolabs.dev</title>
+  </Head>
+)
+
+export default Meta;
