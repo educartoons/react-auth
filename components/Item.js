5ab24785036d4f75c95de00cb9d7434779e2eb71
educartoons <educartoons@gmail.com>
2019-10-03 18:56:33 -0500

    Adding Delete Item from our DB

--- components/Item.js
+++ components/Item.js
@@ -1,47 +1,51 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
-import Title from '../components/styles/Title';
-import ItemStyles from '../components/styles/ItemStyles';
-import PriceTag from '../components/styles/PriceTag';
-import Link from 'next/link';
-import formatMoney from '../lib/formatMoney';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Title from "../components/styles/Title";
+import ItemStyles from "../components/styles/ItemStyles";
+import PriceTag from "../components/styles/PriceTag";
+import Link from "next/link";
+import formatMoney from "../lib/formatMoney";
+import DeleteItem from "./DeleteItem";
 
 class Item extends Component {
-  static propTypes ={
+  static propTypes = {
     item: PropTypes.object.isRequired
-  }
+  };
   render() {
-    const {item} = this.props;
+    const { item } = this.props;
     return (
       <ItemStyles>
         {item.image && <img src={item.image} alt={item.title} />}
         <Title>
-          <Link href={{
-            pathname: '/item',
-            query: {
-              id: item.id
-            }
-          }}>
+          <Link
+            href={{
+              pathname: "/item",
+              query: {
+                id: item.id
+              }
+            }}
+          >
             <a>{item.title}</a>
           </Link>
         </Title>
         <PriceTag>{formatMoney(item.price)}</PriceTag>
         <p>{item.description}</p>
         <div className="buttonList">
-          <Link href={{
-            pathname: 'update',
-            query: {
-              id: item.id
-            }
-          }}>
+          <Link
+            href={{
+              pathname: "update",
+              query: {
+                id: item.id
+              }
+            }}
+          >
             <a>Edit ✏️</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete item</DeleteItem>
         </div>
       </ItemStyles>
-    )
+    );
   }
 }
 
